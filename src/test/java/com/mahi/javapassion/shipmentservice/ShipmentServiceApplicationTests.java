10297bd18c332b75f56ed9bc66c2aa0c94a865c4
Mahesh Yerudkar <1000037803@hexaware.com>
2023-02-25 19:37:14 +0530

    Initial Commit

---
+++ src/test/java/com/mahi/javapassion/shipmentservice/ShipmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahi.javapassion.shipmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
